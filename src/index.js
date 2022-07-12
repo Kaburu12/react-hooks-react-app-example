43d005eaedf598db294d21c5ccac7dc3c6bb67e6
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-12 15:05:53 +0300

    commit tree 4ff7d14bc75f14967e77dc7c3c193c7485b958f1

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const header = <h1 className="main">Hello from React!</h1>;
+
+ReactDOM.render(header, document.querySelector("#container"));
